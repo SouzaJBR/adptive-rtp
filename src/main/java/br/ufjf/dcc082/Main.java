8e4b0dfc4c3d761deb679228164d39f603156b60
João Souza <joao.souza@ice.ufjf.br>
2017-12-05 22:54:44 -0200

    Addes Client tests

--- src/main/java/br/ufjf/dcc082/Main.java
+++ src/main/java/br/ufjf/dcc082/Main.java
@@ -8,4 +8,37 @@ import javax.swing.*;
 
 public class Main {
 
+    private JFrame frame;
+    private EmbeddedMediaPlayerComponent mediaPlayerComponent;
+
+    public static void main(String[] args) {
+        new NativeDiscovery().discover();
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    new Main();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public Main() throws InterruptedException {
+
+        frame = new JFrame("Adptive RTP - Client");
+
+        frame.setBounds(100, 100, 1280, 720);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+        frame.setContentPane(mediaPlayerComponent);
+        frame.setVisible(true);
+        mediaPlayerComponent.getMediaPlayer().playMedia("rtp://@239.0.0.1:5024");
+        Thread.sleep(30000);
+        mediaPlayerComponent.getMediaPlayer().playMedia("rtp://@239.0.0.1:5004");
+        Thread.sleep(30000);
+        mediaPlayerComponent.getMediaPlayer().playMedia("rtp://@239.0.0.1:5014");
+    }
 }
